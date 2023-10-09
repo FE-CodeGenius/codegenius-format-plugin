46b1bec9f821c5f31746c0309da61c2d94416e32
fe-xiaoxin <zxin088@gmail.com>
2023-10-09 11:14:05 +0800

    fix(config): 修复 CI 流程配置

--- codeg.config.mjs
+++ codeg.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "code-genius";
 import { gitInitSimpleHooksInstaller } from "@codegenius/hooks-plugin";
+import { gitCommitVerifyInstaller } from "@codegenius/verify-plugin";
 
 export default defineConfig({
   plugins: [
     gitInitSimpleHooksInstaller(),
+    gitCommitVerifyInstaller(),
   ],
 });
